97a615cc789a412a21981d8db614c22fedb3a088
Yirmel-Sanchez <91554860+Yirmel-Sanchez@users.noreply.github.com>
2022-12-01 13:09:19 +0100

    Métodos del sirviente
    renameTile, addTags, removeTags y funciones auxiliares.

--- iceflix/catalog-server.py
+++ iceflix/catalog-server.py
@@ -18,45 +18,66 @@ class DB_controller():
         except:
             logging.error("Error al cargar la base de datos")
 
-    def guardarMedia(self, media):
+    def guardarMedia(self):
         try:
             with open(self.db_name, 'w') as f:
-                json.dump(media, f, indent=4)
+                json.dump(self.media, f, indent=4)
                 return True
         except:
             logging.error("Error al guardar la base de datos")
             return False
+    
+    def actualizarMedio(self, medioJson):
+        for video in self.media["medios"]:
+            if video["id"] == medioJson["id"]:
+                video["provider"] = medioJson["provider"]
+                video["info"] = medioJson["info"]
+        return self.guardarMedia()
 
 class Catalog(IceFlix.MediaCatalog):
     """Servant for the IceFlix.MediaCatalog interface."""
     
+    # metodos auxiliares
     def __init__(self):
         """ Inicializador del catálogo. """
         self.dbController = DB_controller()
-
-    def getTile(self, mediaId, userToken, current=None):  # pylint:disable=invalid-name, unused-argument
-        "Devuelve el medio cuyo identificador sea igual a mediaId."
-        
-        # Comprobamos que el usuario está autorizado
+    
+    def autenticate(self, userToken):
+        "Indica si el usuario especificado está autorizado."
         main = CatalogServer().serverMain()
         try:
-            authorized = main.getAutenticator().isAuthorized(userToken)
+            return main.getAutenticator().isAuthorized(userToken)
         except IceFlix.TemporaryUnavailable:
             raise IceFlix.TemporaryUnavailable()
 
+    def authorized(self, userToken):
+        "Lanza una excepción si el usuario no está autorizado."
+        authorized = self.autenticate(userToken)
         if not authorized:
             raise IceFlix.Unauthorized()
-        
-        # Comprobamos que el mediaId exista en la base de datos
-        medioAux = None
+
+    def getMediaDB(self, mediaId):
+        "Devuelve el medio cuyo identificador sea igual a mediaId si está en la BD."
         media = self.dbController.media
         for video in media["medios"]:
             if video["id"] == mediaId:
-                medioAux = video
-                break
+                return video
+        raise IceFlix.WrongMediaId()
+
+    def removeTag(self, tagsRem, listTags):
+        "Elimina los tags de la lista listTags."
+        for tag in tagsRem:
+            if tag in listTags:
+                listTags.remove(tag)
+        return listTags
+    
+    # metodos de la interfaz
+    def getTile(self, mediaId, userToken, current=None):  # pylint:disable=invalid-name, unused-argument
+        "Devuelve el medio cuyo identificador sea igual a mediaId."
+        self.authorized(userToken) # Comprobamos que el usuario está autorizado
         
-        if medioAux is None:
-            raise IceFlix.WrongMediaId()
+        # Comprobamos que el mediaId exista en la base de datos
+        medioAux = self.getMediaDB(mediaId)
         
         # Creamos el medio
         medioInfo = IceFlix.MediaInfo()
@@ -67,7 +88,6 @@ class Catalog(IceFlix.MediaCatalog):
         medio.mediaId = medioAux["id"]
         medio.provider = medioAux["provider"]
         medio.info = medioInfo
-
         return medio
         
     def getTilesByName(self, name, exact, current=None):  # pylint:disable=invalid-name, unused-argument
@@ -86,16 +106,7 @@ class Catalog(IceFlix.MediaCatalog):
 
     def getTilesByTags(self, tags, includeAllTags, userToken, current=None):  # pylint:disable=invalid-name, unused-argument
         "Devuelve una lista con los identificadores según los tags de los medios."
-
-        # Comprobamos que el usuario está autorizado
-        main = CatalogServer().serverMain()
-        try:
-            authorized = main.getAutenticator().isAuthorized(userToken)
-        except IceFlix.TemporaryUnavailable:
-            raise IceFlix.TemporaryUnavailable()
-
-        if not authorized:
-            raise IceFlix.Unauthorized()
+        self.authorized(userToken) # Comprobamos que el usuario está autorizado
         
         # obtener los resultados
         result = []
@@ -113,10 +124,6 @@ class Catalog(IceFlix.MediaCatalog):
                     result.append(video["id"])
         
         return result
-    
-    def tagInList(self, tag, list):
-        return tag in list
-
 
     def newMedia(self,  mediaId, provider, current=None):  # pylint:disable=invalid-name, unused-argument
         "el FileService informa que hay medios disponibles."
@@ -130,18 +137,54 @@ class Catalog(IceFlix.MediaCatalog):
     
     def renameTile(self,  mediaId, name, adminToken, current=None):  # pylint:disable=invalid-name, unused-argument
         "el admin lo utiliza para renombrar medios."
-        # TODO: implement
-        return None
+        main = CatalogServer().serverMain()
+        try:
+            esAdmin = main.getAutenticator().isAdmin(adminToken)
+        except IceFlix.TemporaryUnavailable:
+            raise IceFlix.TemporaryUnavailable()
+        
+        if not esAdmin:
+            raise IceFlix.Unauthorized()
+        
+        # Comprobamos que el mediaId exista en la base de datos
+        medioAux = self.getMediaDB(mediaId)
+
+        # Renombramos el medio
+        medioAux["info"]["name"] = name
+
+        # Guardamos los cambios
+        self.dbController.actualizarMedio(medioAux)
+        self.dbController.guardarMedia()
     
     def addTags(self,  mediaId, tags, userToken, current=None):  # pylint:disable=invalid-name, unused-argument
         "permite añadir una lista de tags a un medio concreto."
-        # TODO: implement
-        return None
+        self.authorized(userToken) # Comprobamos que el usuario está autorizado
+
+        # Comprobamos que el mediaId exista en la base de datos
+        medioAux = self.getMediaDB(mediaId)
+
+        # Añadimos los tags
+        listaNueva = medioAux["info"]["tags"] + tags
+        medioAux["info"]["tags"] = list(set(listaNueva))
+        
+        # Guardamos los cambios
+        self.dbController.actualizarMedio(medioAux)
+        self.dbController.guardarMedia()
     
     def removeTags(self,  mediaId, tags, userToken, current=None):  # pylint:disable=invalid-name, unused-argument
         "permite eliminar una lista de tags de un medio concreto."
-        # TODO: implement
-        return None
+        self.authorized(userToken) # Comprobamos que el usuario está autorizado
+
+        # Comprobamos que el mediaId exista en la base de datos
+        medioAux = self.getMediaDB(mediaId)
+
+        # Eliminar los tags
+        listaNueva = self.removeTag(tags, medioAux["info"]["tags"])
+        medioAux["info"]["tags"] = listaNueva
+        
+        # Guardamos los cambios
+        self.dbController.actualizarMedio(medioAux)
+        self.dbController.guardarMedia()
 
 class CatalogServer(Ice.Application):
     """Example Ice.Application for a catalog service."""
@@ -191,10 +234,5 @@ if __name__ == '__main__':
     server_catalog = CatalogServer()
     sys.exit(server_catalog.main(sys.argv))
 
-# StringList getTilesByTags(StringList tags, bool includeAllTags, string userToken) throws Unauthorized;
 # void newMedia(string mediaId, FileService* provider);
 # void removeMedia(string mediaId, FileService* provider);
-# void renameTile(string mediaId, string name, string adminToken) throws Unauthorized, WrongMediaId;
-
-# void addTags(string mediaId, StringList tags, string userToken) throws Unauthorized, WrongMediaId;
-# void removeTags(string mediaId, StringList tags, string userToken) throws Unauthorized, WrongMediaId;
